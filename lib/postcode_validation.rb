cdb5bfb572ce9c056adc8227d2980625c75973c5
Camilla van Klinken <camilla@madetech.com>
2017-04-25 13:28:04 +0100

    rename domain address

--- lib/postcode_validation.rb
+++ lib/postcode_validation.rb
@@ -3,7 +3,7 @@ require 'postcode_validation/version'
 module PostcodeValidation
   require 'httparty'
   require 'postcode_validation/domain/potential_address_match'
-  require 'postcode_validation/domain/address_list'
+  require 'postcode_validation/domain/address'
   require 'postcode_validation/error/request_error'
   require 'postcode_validation/gateway/pca_potential_address_match'
   require 'postcode_validation/gateway/pca_address_list'
