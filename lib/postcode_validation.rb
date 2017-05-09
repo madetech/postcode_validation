4745d3b940a912864685777d33d1c448030ed621
Emile <emile.swarts123@gmail.com>
2017-05-09 11:13:16 +0100

    Remove reference to unused class

--- lib/postcode_validation.rb
+++ lib/postcode_validation.rb
@@ -7,7 +7,6 @@ module PostcodeValidation
   require 'postcode_validation/error/request_error'
   require 'postcode_validation/gateway/pca_potential_address_match'
   require 'postcode_validation/gateway/pca_address_list'
-  require 'postcode_validation/gateway/pca_postcode_to_addresses'
   require 'postcode_validation/use_case/validate_address'
   require 'postcode_validation/use_case/address_list'
 end
