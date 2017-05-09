a3edc5178c63d3144279e46d7013de5df445b7c0
Emile <emile.swarts123@gmail.com>
2017-05-09 10:51:16 +0100

    Refactor tests

--- spec/gateway/pca_address_list_spec.rb
+++ spec/gateway/pca_address_list_spec.rb
@@ -1,64 +1,93 @@
 describe PostcodeValidation::Gateway::PCAAddressList do
+  context 'Address list without further lookups' do
+    context 'GB' do
+      it 'returns the final results' do
+        VCR.use_cassette('PCA-address-list-gb') do
+          subject = described_class.new.query(
+            search_term: 'SE15HG',
+            country: 'GB'
+          )
 
-  context do
-    it 'Searches locally with a postcode that has multiple hits' do
-      search_term = 'SE1 0SW'
-      country = 'GB'
-
-      VCR.use_cassette('PCA-address-list') do
-        subject = described_class.new.query(search_term: search_term, country: country)
-
-        expect(subject[0].street_address).to eq('SE1 0SW, Southwark Street, London - 46 Addresses')
-        expect(subject[0].more_results_id).to eq('GB|RM|ENG|0SW-SE1')
+          expect(subject[0].street_address).to eq('Tesco Stores Ltd, 107 Dunton Road, London, SE1 5HG')
+          expect(subject[0].more_results_id).to eq(nil)
+        end
       end
     end
 
-    it 'Searches locally' do
-      search_term = 'SE1 0SW'
-      country = 'GB'
+    context 'FR' do
+      it 'returns the final results' do
+        VCR.use_cassette('PCA-address-list-fr') do
+          subject = described_class.new.query(
+            search_term: '7552',
+            country: 'FR'
+          )
 
-      VCR.use_cassette('PCA-address-list-by-id') do
-        subject = described_class.new.query(search_term: search_term, country: country, more_results_id: 'GB|RM|ENG|0SW-SE1')
+          expect(subject[0].street_address).to eq('7552 Route des Clues, 06440 Peille')
+          expect(subject[0].more_results_id).to eq(nil)
+        end
+      end
+    end
+  end
 
-        expect(subject[0].street_address).to eq('Abbott Mead Vickers Group Ltd, 90 Southwark Street, London, SE1 0SW')
-        expect(subject[1].street_address).to eq('Code Worldwide Ltd, 90 Southwark Street, London, SE1 0SW')
-        expect(subject[0].more_results_id).to eq(nil)
+  context 'Multiple addresses for a postcode' do
+    context 'GB' do
+      it 'finds a result that needs an additional lookup' do
+        VCR.use_cassette('PCA-postcode-result-gb') do
+          subject = described_class.new.query(
+            search_term: 'SE1 0SW',
+            country: 'GB'
+          )
+
+          expect(subject[0].street_address).to eq('SE1 0SW, Southwark Street, London - 46 Addresses')
+          expect(subject[0].more_results_id).to eq('GB|RM|ENG|0SW-SE1')
+        end
       end
     end
 
-    it 'Searches internationally' do
-      VCR.use_cassette('PCA-address-list-international') do
-        search_term = '7552'
-        country = 'FR'
+    context 'FR' do
+      it 'finds a result that needs an additional lookup' do
+        VCR.use_cassette('PCA-postcode-result-fr') do
+          subject = described_class.new.query(
+            search_term: '75020',
+            country: 'FR'
+          )
 
-        subject = described_class.new.query(search_term: search_term, country: country)
+          expect(subject[0].street_address).to eq('75020, Paris - 95967 Addresses')
+          expect(subject[0].more_results_id).to eq('FR|TT|FRE|75020')
+        end
+      end
+    end
+  end
 
-        expect(subject[0].street_address).to eq('7552 Route des Clues, 06440 Peille')
-        expect(subject[0].more_results_id).to eq(nil)
+  context 'Address lookup given the ID of a previous result' do
+    context 'GB' do
+      it 'returns an address list using a previous result ID' do
+        VCR.use_cassette('PCA-address-list-by-id-gb') do
+          subject = described_class.new.query(
+            search_term: 'SE1 0SW',
+            country: 'GB',
+            more_results_id: 'GB|RM|ENG|0SW-SE1'
+          )
+
+          expect(subject[0].street_address).to eq('Abbott Mead Vickers Group Ltd, 90 Southwark Street, London, SE1 0SW')
+          expect(subject[1].street_address).to eq('Code Worldwide Ltd, 90 Southwark Street, London, SE1 0SW')
+          expect(subject[0].more_results_id).to eq(nil)
+        end
       end
     end
 
-    it 'Searches internationally' do
-      VCR.use_cassette('PCA-address-list-international-id') do
-        search_term = '75020'
-        country = 'FR'
+    context 'FR' do
+      it 'returns an address list using a previous result ID' do
+        VCR.use_cassette('PCA-address-list-by-id-fr') do
+          subject = described_class.new.query(
+            search_term: '75020',
+            country: 'FR',
+            more_results_id: 'FR|TT|FRE|75020'
+          )
 
-        subject = described_class.new.query(search_term: search_term, country: country)
-
-        expect(subject[0].street_address).to eq('75020, Paris - 95967 Addresses')
-        expect(subject[0].more_results_id).to eq('FR|TT|FRE|75020')
-      end
-    end
-
-    it 'Searches internationally' do
-      VCR.use_cassette('PCA-address-list-international-id-2') do
-        search_term = '75020'
-        country = 'FR'
-
-        subject = described_class.new.query(search_term: search_term, country: country, more_results_id: 'FR|TT|FRE|75020')
-
-        expect(subject[0].street_address).to eq('52 Avenue Gambetta, 20e Arrondissement Paris, 75020 Paris')
-        expect(subject[0].more_results_id).to eq(nil)
+          expect(subject[0].street_address).to eq('52 Avenue Gambetta, 20e Arrondissement Paris, 75020 Paris')
+          expect(subject[0].more_results_id).to eq(nil)
+        end
       end
     end
   end
