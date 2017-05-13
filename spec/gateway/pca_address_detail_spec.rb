173399196f0157de5a8c8bb145ff0d777799ece8
Emile <emile.swarts123@gmail.com>
2017-05-13 12:37:38 +0100

    Use MadeTech for address lookup in the specs

--- spec/gateway/pca_address_detail_spec.rb
+++ spec/gateway/pca_address_detail_spec.rb
@@ -2,11 +2,12 @@ describe PostcodeValidation::Gateway::PCAAddressDetail do
   context 'GB' do
     it 'returns the final results' do
       VCR.use_cassette('PCA-address-detail-gb') do
-        subject = described_class.new.find(id: 'GB|RM|A|8224138')
+        subject = described_class.new.find(id: 'GB|RM|A|54284042')
 
-        expect(subject.address_line_1).to eq('755/2 Ferry Road')
+        expect(subject.company).to eq('Made Tech')
+        expect(subject.address_line_1).to eq('136 Southwark Street')
         expect(subject.address_line_2).to eq('')
-        expect(subject.city).to eq('Edinburgh')
+        expect(subject.city).to eq('London')
         expect(subject.country).to eq('United Kingdom')
       end
     end
