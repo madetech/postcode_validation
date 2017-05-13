82cd0f25a8d91fc3767faeae8c72bade268f8d98
Emile <emile.swarts123@gmail.com>
2017-05-13 12:40:22 +0100

    Reword PCAAddressList contexts

--- spec/gateway/pca_address_list_spec.rb
+++ spec/gateway/pca_address_list_spec.rb
@@ -1,15 +1,13 @@
 describe PostcodeValidation::Gateway::PCAAddressList do
-  context 'Address list without further lookups' do
-    context 'GB' do
-      it 'returns the final results' do
-        VCR.use_cassette('PCA-address-list-gb') do
-          subject = described_class.new.query(
-            search_term: 'SE15HG',
-            country: 'GB'
-          )
+  context 'GB' do
+    it 'returns the URL for retrieving address details' do
+      VCR.use_cassette('PCA-address-list-gb') do
+        subject = described_class.new.query(
+          search_term: 'SE15HG',
+          country: 'GB'
+        )
 
-          expect(subject[0].url).to eq('https://services.postcodeanywhere.co.uk/Capture/Interactive/Retrieve/1.00/json.ws?Id=GB|RM|A|21456992&Key=FAKEKEY')
-        end
+        expect(subject[0].url).to eq('https://services.postcodeanywhere.co.uk/Capture/Interactive/Retrieve/1.00/json.ws?Id=GB|RM|A|21456992&Key=FAKEKEY')
       end
     end
   end
